e80a06c9ee003bb78d90e808ae6a6b3d46f6f955
moontai0724 <moontai0724@gmail.com>
2024-07-18 09:30:53 +0800

    refactor(utils): dry out prepareParams method and open options for customize
    - ability to set http method and endpoint
    - option for add json format instruct or not

--- src/common/request/request-rest.ts
+++ src/common/request/request-rest.ts
@@ -1,41 +1,7 @@
 import { handleRestResponse } from "./handle-rest-response";
-import { OAuthURLSearchParams } from "./oauth";
+import { prepareParams } from "./prepare-params";
 
 const endpoint = "https://api.flickr.com/services/rest";
-
-async function prepareParams(
-  credentials: Credentials | undefined,
-  params: Record<PropertyKey, string> | URLSearchParams,
-) {
-  const urlSearchParams = new OAuthURLSearchParams(params);
-
-  urlSearchParams.set("format", "json");
-  urlSearchParams.set("nojsoncallback", "1");
-
-  // No credentials, return the params without keys
-  if (!credentials?.apiKey) return urlSearchParams;
-
-  // With API key only, no OAuth tokens
-  if ("consumerSecret" in credentials === false) {
-    urlSearchParams.set("api_key", credentials.apiKey);
-
-    return urlSearchParams;
-  }
-
-  // OAuth
-  return urlSearchParams.sign(
-    "GET",
-    endpoint,
-    {
-      token: credentials.apiKey,
-      secret: credentials.consumerSecret,
-    },
-    {
-      token: credentials.oauthToken!,
-      secret: credentials.oauthTokenSecret!,
-    },
-  );
-}
 
 export async function requestRest<T>(
   credentials: Credentials | undefined,
@@ -43,7 +9,11 @@ export async function requestRest<T>(
   key?: string,
 ) {
   const url = new URL(endpoint);
-  const urlSearchParams = await prepareParams(credentials, params as never);
+  const urlSearchParams = await prepareParams({
+    credentials,
+    endpoint,
+    params,
+  });
 
   url.search = urlSearchParams.toString();
 
