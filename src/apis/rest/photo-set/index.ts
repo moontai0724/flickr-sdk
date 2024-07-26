44fa8ed6b9db6be85ed4e71fb18426fa4b096e69
moontai0724 <moontai0724@gmail.com>
2024-07-27 00:25:37 +0800

    feat(photosets.delete): implement apis

--- src/apis/rest/photo-set/index.ts
+++ src/apis/rest/photo-set/index.ts
@@ -1,4 +1,9 @@
 export * from "./add-photo";
 export * from "./create";
+export {
+  deletePhotoset as delete,
+  type DeleteOptions,
+  type DeleteResponse,
+} from "./delete";
 export * from "./get-info";
 export * from "./get-list";
