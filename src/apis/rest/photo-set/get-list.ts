a97fdd8f7dcacdb33b2f385347c3f43a757fb0a7
moontai0724 <moontai0724@gmail.com>
2024-07-26 23:56:09 +0800

    refactor(photosets.getList): rearrange code and docs

--- src/apis/rest/photo-set/get-list.ts
+++ src/apis/rest/photo-set/get-list.ts
@@ -1,7 +1,33 @@
 import { requestRest } from "common/request";
-import type { Paginated, PhotoSet, WithCredentials } from "types";
+import type { License, Paginated, Photo, WithCredentials } from "types";
+
+export type PrimaryPhotoExtras =
+  | "license"
+  | "date_upload"
+  | "date_taken"
+  | "owner_name"
+  | "icon_server"
+  | "original_format"
+  | "last_update"
+  | "geo"
+  | "tags"
+  | "machine_tags"
+  | "o_dims"
+  | "views"
+  | "media"
+  | "path_alias"
+  | "url_sq"
+  | "url_t"
+  | "url_s"
+  | "url_m"
+  | "url_o";
 
 export interface GetListOptions extends WithCredentials {
+  /**
+   * The NSID of the user to get a photoset list for. If none is specified, the
+   * calling user is assumed.
+   */
+  userId?: string;
   /**
    * The page of results to get. Currently, if this is not provided, all sets
    * are returned, but this behaviour may change in future.
@@ -13,21 +39,16 @@ export interface GetListOptions extends WithCredentials {
    */
   perPage?: number;
   /**
-   * A comma-separated list of photo ids. If specified, each returned set will
-   * include a list of these photo ids that are present in the set as
-   * "has_requested_photos"
+   * A list of extra information to fetch for the primary photo.
    */
-  photoIds?: string;
+  primaryPhotoExtras?: (PrimaryPhotoExtras | (string & NonNullable<unknown>))[]; // preserve possibility to any string
   /**
-   * A comma-delimited list of extra information to fetch for the primary photo.
-   * Currently supported fields are: `license`, `date_upload`, `date_taken`,
-   * `owner_name`, `icon_server`, `original_format`, `last_update`, `geo`,
-   * `tags`, `machine_tags`, `o_dims`, `views`, `media`, `path_alias`, `url_sq`,
-   * `url_t`, `url_s`, `url_m`, `url_o`
+   * A list of photo ids. If specified, each returned set will include a list of
+   * these photo ids that are present in the set as "has_requested_photos"
    */
-  primaryPhotoExtras?: string;
+  photoIds?: string[];
   /**
-   * A comma-separated list of groups used to sort the output sets. If has_photo
+   * A list of groups used to sort the output sets. If has_photo
    * is present, any of the calling user's galleries containing photos referred
    * to in photo_ids will be returned before other galleries. The order of the
    * sort_groups will dictate the order that the groups are returned in. Only
@@ -35,15 +56,192 @@ export interface GetListOptions extends WithCredentials {
    * "sort_group" parameter indicating the sort_group that each set is part of,
    * or null if not applicable
    */
-  sortGroups?: string;
+  sortGroups?: string[];
+}
+
+interface ListPhotoSetItem {
+  extras: Pick<Photo, "license" | "dateuploaded">;
+  primaryPhotoExtras?: {
+    license: License;
+    /**
+     * Unix timestamp of when the photo was uploaded.
+     * @example 1683055233
+     * @see https://www.flickr.com/services/api/misc.dates.html
+     */
+    dateupload: string;
+    /**
+     * Unix timestamp of when the photo was last updated.
+     * @example 1683055302
+     * @see https://www.flickr.com/services/api/misc.dates.html
+     */
+    lastupdate: string;
+    /**
+     * Date time when the photo was taken in (From Official) MySQL `datetime` format.
+     * @example "2023-04-30 17:59:44"
+     * @see https://www.flickr.com/services/api/misc.dates.html
+     */
+    datetaken: "2022-12-10 18:27:37";
+    /**
+     * (From Official) The accuracy to which we know the date to be true. At present, the following granularities are used:
+     * - 0: Y-m-d H:i:s
+     * - 4: Y-m
+     * - 6: Y
+     * - 8: Circa...
+     */
+    datetakengranularity: number;
+    datetakenunknown: string;
+    /**
+     * The display name of the user account of the owner of the set.
+     * @example "月太げつたい"
+     */
+    ownername: string;
+    /**
+     * The server id to access the photo.
+     * @example "65535"
+     * @see https://www.flickr.com/services/api/misc.urls.html
+     */
+    iconserver: string;
+    /**
+     * The icon farm id of the photo.
+     * @example 66
+     * @see https://www.flickr.com/services/api/misc.buddyicons.html
+     */
+    iconfarm: string;
+    /**
+     * The number of views of the photo.
+     * @example 550
+     */
+    views: number;
+    /**
+     * Space separated list of tags.
+     * @example "cosplay pf pf38"
+     */
+    tags: string;
+    machineTags: string;
+    /**
+     * The secret key to get the original photo.
+     * @example "a22165bf3f"
+     * @see https://www.flickr.com/services/api/misc.urls.html
+     */
+    originalsecret: string;
+    /**
+     * @example "jpg"
+     */
+    originalformat: string;
+    /**
+     * seems to be 0 if unknown
+     */
+    latitude: number;
+    /**
+     * seems to be 0 if unknown
+     */
+    longitude: number;
+    /**
+     * seems to be 0 if unknown
+     */
+    accuracy: number;
+    /**
+     * seems to be 0 if unknown
+     */
+    context: number;
+    /**
+     * @example "photo"
+     */
+    media: string;
+    /**
+     * @example "ready"
+     */
+    mediaStatus: string;
+    /**
+     * Direct image url to the sq (square thumbnail) size of the photo.
+     * @example "https://live.staticflickr.com/65535/52865522979_5c6b5e0405_s.jpg"
+     */
+    urlSq: string;
+    /**
+     * The height of the sq (square thumbnail) size of the photo.
+     * @example 75
+     */
+    heightSq: number;
+    /**
+     * The width of the sq (square thumbnail) size of the photo.
+     * @example 75
+     */
+    widthSq: number;
+    /**
+     * Direct image url to the t (thumbnail) size of the photo.
+     * @example "https://live.staticflickr.com/65535/52865522979_5c6b5e0405_t.jpg"
+     */
+    urlT: string;
+    /**
+     * The height of the t (thumbnail) size of the photo.
+     * @example 67
+     */
+    heightT: number;
+    /**
+     * The width of the t (thumbnail) size of the photo.
+     * @example 100
+     */
+    widthT: number;
+    /**
+     * Direct image url to the s (small) size of the photo.
+     * @example "https://live.staticflickr.com/65535/52865522979_5c6b5e0405_m.jpg"
+     */
+    urlS: string;
+    /**
+     * The height of the s (small) size of the photo.
+     * @example 160
+     */
+    heightS: number;
+    /**
+     * The width of the s (small) size of the photo.
+     * @example 240
+     */
+    widthS: number;
+    /**
+     * Direct image url to the m (medium) size of the photo.
+     * @example "https://live.staticflickr.com/65535/52865522979_5c6b5e0405_m.jpg"
+     */
+    urlM: string;
+    /**
+     * The height of the m (medium) size of the photo.
+     * @example 333
+     */
+    heightM: number;
+    /**
+     * The width of the m (medium) size of the photo.
+     * @example 500
+     */
+    widthM: number;
+    /**
+     * Direct image url to the o (original) size of the photo.
+     * @example "https://live.staticflickr.com/65535/52865522979_a22165bf3f_o.jpg"
+     */
+    urlO: string;
+    /**
+     * The height of the o (original) size of the photo.
+     * @example 4000
+     */
+    heightO: number;
+    /**
+     * The width of the o (original) size of the photo.
+     * @example 6000
+     */
+    widthO: number;
+    /**
+     * Should be the user alias ID of the owner of the set.
+     * @example "moontai0724"
+     */
+    pathalias: string;
+  };
   /**
-   * The NSID of the user to get a photoset list for. If none is specified, the
-   * calling user is assumed.
+   * A list of photo ids. If specified, each returned set will include a list of
+   * these photo ids that are present in the set as "has_requested_photos"
    */
-  userId?: string;
+  hasRequestedPhotos?: string[];
 }
 
 export interface GetListResponse extends Paginated {
+  cancreate: boolean;
   /**
    * List of photosets.
    *
@@ -51,7 +249,7 @@ export interface GetListResponse extends Paginated {
    * the newest set is first. Applications displaying photosets should respect
    * the user's ordering.
    */
-  photoset: PhotoSet[];
+  photoset: ListPhotoSetItem[]; // TODO: make this type correspond to the request
 }
 
 /**
@@ -59,27 +257,25 @@ export interface GetListResponse extends Paginated {
  *
  * @see https://www.flickr.com/services/api/flickr.photosets.getList.html
  */
-export async function getList(options: GetListOptions) {
-  const {
-    credentials,
-    page,
-    perPage,
-    photoIds,
-    primaryPhotoExtras,
-    sortGroups,
-    userId,
-  } = options;
-
+export async function getList({
+  credentials,
+  page,
+  perPage,
+  photoIds,
+  primaryPhotoExtras,
+  sortGroups,
+  userId,
+}: GetListOptions) {
   return requestRest<GetListResponse>({
     credentials,
     params: {
       method: "flickr.photosets.getList",
-      page: page ? page.toString() : undefined,
-      per_page: perPage ? perPage.toString() : undefined,
-      photo_ids: photoIds || undefined,
-      primary_photo_extras: primaryPhotoExtras || undefined,
-      sort_groups: sortGroups || undefined,
-      user_id: userId || undefined,
+      page: page?.toString(),
+      per_page: perPage?.toString(),
+      photo_ids: photoIds?.join(","),
+      primary_photo_extras: primaryPhotoExtras?.join(","),
+      sort_groups: sortGroups?.join(","),
+      user_id: userId,
     },
     key: "photosets",
   });
