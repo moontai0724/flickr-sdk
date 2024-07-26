89b528b1368bd152caa0092c67a87e9dba6735ec
moontai0724 <moontai0724@gmail.com>
2024-07-26 22:27:09 +0800

    feat(photosets.addPhoto): implement apis

---
+++ src/apis/rest/photo-set/add-photo.ts
@@ -0,0 +1,39 @@
+import { requestRest } from "common/request";
+import type { WithOAuthCredentials } from "types";
+
+export interface AddPhotoOptions extends WithOAuthCredentials {
+  /**
+   * The id of the photoset to add a photo to.
+   */
+  photosetId: string;
+  /**
+   * The id of the photo to add to the set.
+   */
+  photoId: string;
+}
+
+export type AddPhotoResponse = void;
+
+/**
+ * Add a photo to the end of an existing photoset.
+ *
+ * This method requires authentication with 'write' permission.
+ *
+ * @returns empty success response if it completes without error.
+ *
+ * @see https://www.flickr.com/services/api/flickr.photosets.addPhoto.html
+ */
+export async function addPhoto({
+  credentials,
+  photosetId,
+  photoId,
+}: AddPhotoOptions) {
+  return requestRest<AddPhotoResponse>({
+    credentials,
+    params: {
+      method: "flickr.photosets.addPhoto",
+      photoset_id: photosetId,
+      photo_id: photoId,
+    },
+  });
+}
--- src/apis/rest/photo-set/index.ts
+++ src/apis/rest/photo-set/index.ts
@@ -1,3 +1,4 @@
+export * from "./add-photo";
 export * from "./create";
 export * from "./get-info";
 export * from "./get-list";
